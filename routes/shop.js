77fb0be262380b3eb7c5b77a1ace19375dd6aa4a
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-03-13 10:56:42 +0530

    Outputting Dynamic Content & implemented pug templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,11 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     //console.log('This always run!');
-    console.log('shop.js',adminData.products);
+    console.log('shop.js', adminData.products);
     //res.send('<h1>This always run</h1>')
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products;
+    res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
