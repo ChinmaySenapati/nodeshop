7a49a940b2b39f65358b14448e909853408dc507
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-03-13 12:26:49 +0530

    added active path in views & set page title dynamically

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get('/', (req, res, next) => {
     //res.send('<h1>This always run</h1>')
     //res.sendFile(path.join(rootDir, 'views', 'shop.html'))
     const products = adminData.products;
-    res.render('shop', { prods: products, docTitle: 'Shop' });
+    res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
