051c22a7591aeb357f643caa6a4ce3052d5759c8
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-03-13 11:34:00 +0530

    converting all html files into pug templating engine

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ app.use(shopRoutes)
 
 app.use((req,res,next) => {
     //res.status(404).send('<h1>Page Not Found!</h1>')
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    //res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    res.status(404).render('404');
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,8 @@ const products = [];
 router.get('/add-product', (req,res,next) => {
     console.log('<h1>This is from "add products" page</h1>');
     //res.send("<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Send</button></form>")
-    res.sendFile((path.join(rootDir,'views', 'add-product.html')))
+    //res.sendFile((path.join(rootDir,'views', 'add-product.html')))
+    res.render('add-product', {pageTitle: "Add Product"})
 })
 
 router.post('/add-product', (req, res, next) => {
