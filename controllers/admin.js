ad31aa7503058d0d0c952f5ca75f712604ac0807
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-03-14 14:12:22 +0530

    storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,8 +2,8 @@ const Product = require('../models/product')
 
 exports.getAddProduct = (req,res,next) => {
     res.render(
-        'admin/add-product', 
-        { pageTitle: "Add Product", 
+        'admin/add-product', {
+        pageTitle: "Add Product", 
         path: '/admin/add-product', 
         activeAddProduct: true, 
         productCSS: true, 
@@ -12,7 +12,11 @@ exports.getAddProduct = (req,res,next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product( title, imageUrl, price, description );
     product.save();
     res.redirect('/');
 };
