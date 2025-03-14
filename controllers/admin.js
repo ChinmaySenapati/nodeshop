21696b7daaea1ef306b91a5e90ce914f9437df8e
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-03-14 12:08:18 +0530

    registering the routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,16 +18,12 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+    Product.fetchAll(products => {
         res.render(
-            'shop/product-list', 
-            { prods: products, 
-            pageTitle: 'Shop', 
-            path: '/', 
-            hasProducts: products.length > 0, 
-            activeShop: true, 
-            productCSS: true 
+            'admin/products', {
+            prods: products, 
+            pageTitle: 'Admin Products', 
+            path: '/admin/products', 
         });
     });
-    
-};
+}
