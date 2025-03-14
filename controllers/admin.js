31fcbf2210593a8f040841a948e0252a29ee1f73
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-03-14 14:46:50 +0530

    Displaying Product Data imp

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,8 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product( title, imageUrl, price, description );
+    console.log('price:', price, 'description', description )
+    const product = new Product( title, imageUrl, description, price );
     product.save();
     res.redirect('/');
 };
