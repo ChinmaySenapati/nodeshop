a30c45021388c04a1610bd3c70c011ca1fb6d9e3
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-01 13:25:59 +0530

    Extracting Dynamic Params (important)

--- controllers/error.js
+++ controllers/error.js
@@ -1,4 +1,3 @@
-exports.get404 = (req,res,next) => {
-    //res.status(404).render('404', {pageTitle: 'Page Not Found',  path:"error"}); sameoutput no change with below path change
-    res.status(404).render('404', {pageTitle: 'Page Not Found',  path:"/path"});
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
