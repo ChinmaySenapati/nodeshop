6e85196d47f09f5177c8612dcb491ef1d43d8ed7
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-05 10:00:26 +0530

    creating & fetching a cart

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ Cart.belongsToMany(Product, { through: CartIthem} );
 Product.belongsToMany(Cart, { through: CartIthem} );
 
 sequelize
-.sync({ force: true }) //used force: true to drop and create DB 
-//.sync()
+//.sync({ force: true }) //used force: true to drop and create DB 
+.sync()
 .then(result => {
     return User.findByPk(1);
     //console.log(result);
@@ -57,6 +57,9 @@ sequelize
 })
 .then(user => {
     //console.log(user);
+    return user.createCart();
+}) 
+.then(cart =>{
     app.listen(3000);
 })
 .catch(err => {
