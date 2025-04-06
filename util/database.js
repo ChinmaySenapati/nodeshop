b08f052cecbbbd8709e83bdbd8a571c820ddc8aa
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-06 10:10:42 +0530

    modifying mongodb database connection & using the connection

--- util/database.js
+++ util/database.js
@@ -1,14 +1,27 @@
 const mongodb = require('mongodb');
-
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-MongoClient.connect('mongodb+srv://chinmays:EvhBfpV8JX2Lylev@cluster0.bcmob3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+MongoClient.connect('mongodb+srv://chinmays:EvhBfpV8JX2Lylev@cluster0.bcmob3z.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
     .then(client => {
         console.log("Connected!");
-        callback(client);
+        _db = client.db();
+        callback();
     })
-    .catch(err => console.log(err));
-}
+    .catch(err => {
+        console.log(err);
+        throw err;
+    });
+};
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
