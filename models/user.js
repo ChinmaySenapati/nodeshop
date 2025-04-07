8762b68bcbe27924c4df3df3e560c3df81e027f0
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-07 12:30:39 +0530

    creating User model & connecting to Mongo DB

--- models/user.js
+++ models/user.js
@@ -1,16 +1,24 @@
-const Sequelize = require('sequelize');
+const { ObjectId } = require('mongodb');
+const getDb = require('../util/database');
 
-const sequelize = require('../util/database');
+class User {
+    constructor(username, email) {
+        this.username = username;
+        this.email = email;
+    }
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+    save() {
+       const db = getDb();
+       return db.collection('users').insertOne(this);
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users')
+        .findOne({ _id: new ObjectId(userId)})
+        // .find({ _id: new ObjectId(userId)})
+        // .next();
+    }
+}
 
 module.exports = User;
