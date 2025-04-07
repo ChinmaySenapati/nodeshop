bf471bdb54050f0235fe8db599fa3f42cb01aad2
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-07 14:12:15 +0530

    displaying the cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -56,55 +56,26 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user.getCart()
-  .then(cart =>{
-    //console.log(cart);
-    return cart.getProducts()
-    .then(products => {
+  .then(products =>{
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
         products: products
       });
     })
-    .catch(err => console.log(err));
-  })
   .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId).then(product => {
-    return req.user.addToCart(product);
-  }).then(result => {
+  Product.findById(prodId)
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
     console.log("postCart method::",result);
+    res.redirect('/cart');
   })
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user.getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: {id: prodId} });
-  //   })
-  //   .then(products =>{
-  //     let product;
-  //     if(products.length > 0) {
-  //       product = products[0];
-  //     }
-  //     //checks & add if products already exist inside cart.
-  //     if(product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId)
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, { through: { quantity: newQuantity} });
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
