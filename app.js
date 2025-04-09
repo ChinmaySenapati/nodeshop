1b10b91db656ae092edc896d52e111549d9d67dc
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-09 20:16:36 +0530

    saving data into mongoDB through mongoose into products collection

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+//const User = require('./models/user');
 
 const uri = process.env.MONGODB_URI;
 
@@ -21,14 +21,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('67f376a403c3c3af8504d5b5')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('67f376a403c3c3af8504d5b5')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
