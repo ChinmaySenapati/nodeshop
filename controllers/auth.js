5594c99f6fa2e514d49eaa781a2493a9bfe79fb9
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-10 14:35:51 +0530

    configuring Cookies with various options

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,20 @@
 exports.getLogin = (req, res, next) => {
-const isLoggedIn = req
-        .get('Cookie')
-        .split(';')[2]
-        .trim()
-        .split('=')[1] === 'true';
+// const isLoggedIn = req
+//         .get('Cookie')
+//         .split(';')[2]
+//         .trim()
+//         .split('=')[1] === 'true';
       res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
       });
 };
 
 exports.postLogin = (req, res, next) => {
+    //res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=20');
+    //res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+    //res.setHeader('Set-Cookie', 'loggedIn=true; Secure');
     res.setHeader('Set-Cookie', 'loggedIn=true');
     res.redirect('/');
 };
