2510096eb5e767e02fe89dfbbeffa31a0bc6385a
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-11 20:56:35 +0530

    adding the signin functionality(important)

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('67f69a4826367ac89c3439db')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 };
@@ -35,29 +51,29 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-
   User.findOne({ email: email })
-  .then(userDoc => {
-    if (userDoc) {
-      return res.redirect('/signup');
-    }
-    return bcrypt.hash(password, 12)
-      .then(hashedPassword => {
-        const user = new User({
-          email: email,
-          password: hashedPassword,
-          cart: { items: [] }
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          console.log('User created');
+          res.redirect('/login');
         });
-        return user.save();
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .then(result => {
-    console.log('User created');
-    res.redirect('/login');
-  })
-  .catch(err => {
-    console.log(err);
-  })
 };
 
 exports.postLogout = (req, res, next) => {
