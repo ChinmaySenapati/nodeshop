9581d6f73de926fd241b15b9c7bc37ade07cd09e
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-15 21:10:26 +0530

    adding validators for password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 const User = require('../models/user');
 
 const authController = require('../controllers/auth');
@@ -13,17 +13,25 @@ router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 
 router.post(
-    '/signup', 
+    '/signup', [
     check('email')
     .isEmail()
     .withMessage('Please enter a valid email address')
     .custom(async (value, { req }) => {
         const user = await User.findOne({ email: value });
         if (user) {
-            throw new Error('Email already exists.');
+            throw new Error('Email already exists. Please choose a different email address.');
         }
     }), 
-    authController.postSignup);
+    body(
+        'password', 
+        'Please enter a password with only numbers & text and Password must be at least 5 characters long.'
+    )
+    .isLength({ min: 5 })
+    .isAlphanumeric(),
+    //body('confirmPassword')
+],
+authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
