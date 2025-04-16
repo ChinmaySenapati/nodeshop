3daff4f4a43284b6eac5252fd598515f412f45bc
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-16 13:46:20 +0530

    validation product addition(important)

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
@@ -14,11 +15,53 @@ router.get('/add-product', isAuth, adminController.getAddProduct);
 router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product', [
+    check('title')
+        .isLength({ min: 3 })
+        .trim()
+        .notEmpty()
+        .withMessage('Title must not be empty')
+        .isAlphanumeric,
+    body('imageUrl')
+       .notEmpty()
+       .withMessage('Image URL must not be empty')
+       .isURL()
+       .trim()
+       .isLength({ max: 2083 }), // Maximum URL length
+    body('price')
+       .isFloat()
+       .withMessage('Price must be a number'),
+    body('description')
+       .notEmpty()
+       .withMessage('Description must not be empty')
+       .isLength({ min: 5, max: 500 })
+       .trim()
+], isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product', [
+    body('title')
+       .isString()
+       .isLength({ min: 3 })
+       .trim()
+       .notEmpty()
+       .withMessage('Title must not be empty'),
+    body('imageUrl')
+       .notEmpty()
+       .withMessage('Image URL must not be empty')
+       .isURL()
+       .trim()
+       .isLength({ max: 2083 }), // Maximum URL length
+    body('price')
+       .isFloat()
+       .withMessage('Price must be a number'),
+    body('description')
+       .notEmpty()
+       .withMessage('Description must not be empty')
+       .isLength({ min: 5, max: 500 })
+       .trim()
+], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
