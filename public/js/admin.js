4319bec620fadf3fc997692d80a97e9fcf96f053
ChinmaySenapati <chinmaysenapati07@gmail.com>
2025-04-18 11:24:08 +0530

    adding client side JS code & sending and handling background requests using JSON(IMPORTANT)

---
+++ public/js/admin.js
@@ -0,0 +1,14 @@
+const deleteProduct =  (btn) => {
+ const prodId = btn.parentNode.querySelector('[name=productId]').value;
+ const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+ fetch('/admin/product/' + prodId, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrf
+    }
+ }).then(result => {
+    console.log(result);
+ }).catch(err => console.log(err));
+};
